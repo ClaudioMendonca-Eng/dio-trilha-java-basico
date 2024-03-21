a9e2e230858b54e7ad14505a6af1d1b4f3fa06b5
Claudio Mendonça <contato@claudiomendonca.eng.br>
2024-03-21 19:32:22 -0300

    Condição ternária

--- 02-escopo-e-estruturas-de-controle/controle-flux/src/ResultadoEscolar.java
+++ 02-escopo-e-estruturas-de-controle/controle-flux/src/ResultadoEscolar.java
@@ -11,14 +11,23 @@ public class ResultadoEscolar {
         System.out.println("Reprovado");
         */
 
-        // Condicional encadeada
+        /** Condicional encadeada
         if(nota >= 7)
             System.out.println("Aprovado");
         else if(nota >= 5 && nota < 7  )
             System.out.println("Recuperação");
         else
             System.out.println("Reprovado");
+        */
 
+        /** Condição ternária simples
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+        */
 
+        // Condição ternária composta
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+               
     }
 }
