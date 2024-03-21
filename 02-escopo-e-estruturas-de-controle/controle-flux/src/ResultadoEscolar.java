37def5afc2aa61024cb088f1bbbc1c45ff396039
Claudio Mendonça <contato@claudiomendonca.eng.br>
2024-03-21 19:25:03 -0300

    Condicional encadeada

--- 02-escopo-e-estruturas-de-controle/controle-flux/src/ResultadoEscolar.java
+++ 02-escopo-e-estruturas-de-controle/controle-flux/src/ResultadoEscolar.java
@@ -3,10 +3,22 @@ public class ResultadoEscolar {
 
        int nota = 6;
        
+       /** Modo Condicional composta
        if(nota >= 7)
         System.out.println("Aprovado");
 
        else
         System.out.println("Reprovado");
+        */
+
+        // Condicional encadeada
+        if(nota >= 7)
+            System.out.println("Aprovado");
+        else if(nota >= 5 && nota < 7  )
+            System.out.println("Recuperação");
+        else
+            System.out.println("Reprovado");
+
+
     }
 }
